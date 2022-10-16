7e4986e7f99476cfca316705e9723d2f20beddba
Yi Ching Wang <doo8409255@gmail.com>
2022-10-16 23:31:24 +0800

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,37 @@
+$('#navbar').load('/pages/share/navbar.html', function (data) {
+    $('#navbar').replaceWith(data);
+    const burger = document.querySelector('.burger')
+    const navLinks = document.querySelector('ul.nav-links')
+    const nav = document.querySelector('.nav')
+    burger.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+        $('body').toggleClass('overflow-hidden');
+    })
+
+    window.addEventListener('scroll', function () {
+        if (document.documentElement.scrollTop > 200) {
+            nav.classList.add('active')
+        } else {
+            nav.classList.remove('active')
+        }
+    })
+});
+
+
+$('#footer').load('/pages/share/footer.html', function (data) {
+    $('#footer').replaceWith(data);
+
+    const scrollTop = $(".scroll-top");
+    if (scrollTop) {
+        const togglescrollTop = function () {
+            window.scrollY > 100
+                ? scrollTop.addClass("active")
+                : scrollTop.removeClass("active");
+        };
+        $(document).on("scroll", togglescrollTop);
+        scrollTop.on("click", function (e) {
+            e.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, 800);
+        });
+    }
+});
