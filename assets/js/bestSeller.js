d0f257ab7b5f91508c2af83edd5b28f64d2b28a9
Yi Ching Wang <doo8409255@gmail.com>
2022-10-21 23:42:54 +0800

    UI

--- assets/js/bestSeller.js
+++ assets/js/bestSeller.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
     $('.nav #bestSeller').addClass('active');
+    $(document).on('input', '.favInput', function () {
+        if ($(this).is(':checked')) {
+            console.log('已加入我的收藏');
+        } else {
+            console.log('已取消收藏');
+        }
+    });
+
+    $('.lazyload').each(function () {
+        var lazy = $(this);
+        var src = lazy.attr('data-src');
+        lazy.css('background-image', 'url("' + src + '")');
+    });
+
+    $('a[data-fancybox="gallery"]').fancybox({
+        buttons: ["close"]
+    });
 });
