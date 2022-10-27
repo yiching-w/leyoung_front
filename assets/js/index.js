f113b60f188e3f3c13b11b53cbce4277cadf3197
Yi Ching Wang <doo8409255@gmail.com>
2022-10-27 22:44:58 +0800

    index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,46 @@
 $(document).ready(function () {
+    const html = document.documentElement;
+    const canvasWrap = document.querySelector(".canvasWrap");
+    const canvas = document.getElementById("hero-lightpass");
+    const context = canvas.getContext("2d");
 
+    const frameCount = 207;
+    const currentFrame = index => (
+        `/assets/image/index/1_00${index < 100 ? index < 10 ? '00' + index : '0' + index : index}.jpg`
+    )
+
+    const preloadImages = () => {
+        for (let i = 1; i < frameCount; i++) {
+            const img = new Image();
+            img.src = currentFrame(i);
+        }
+    };
+
+    const img = new Image()
+    img.src = currentFrame(0);
+    canvas.width = 1158;
+    canvas.height = 770;
+    img.onload = function () {
+        context.drawImage(img, 0, 0);
+    }
+
+    const updateImage = index => {
+        img.src = currentFrame(index);
+        context.drawImage(img, 0, 0);
+    }
+
+    window.addEventListener('scroll', () => {
+        const scrollTop = html.scrollTop;
+        const maxScrollTop = html.scrollHeight - window.innerHeight - 2000;
+
+        const scrollFraction = scrollTop / maxScrollTop;
+        const frameIndex = Math.min(
+            frameCount - 1,
+            Math.ceil(scrollFraction * frameCount)
+        );
+
+        requestAnimationFrame(() => updateImage(frameIndex + 1))
+    });
+
+    preloadImages()
 });
